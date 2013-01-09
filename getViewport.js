a4be0b23060a2e858dc82a6b46c9fe1b3ffd7d61
Brett Jankord <bjankord@gmail.com>
2013-01-09 06:43:00 -0800

    Create getViewport.js

---
+++ getViewport.js
@@ -0,0 +1,20 @@
+/*! Accurate Viewport Dimensions | Author: Brett Jankord, 2012 | License: MIT */
+/* https://github.com/bjankord/Accurate-Viewport */
+﻿
+var getViewport = function(){
+
+    var vpw, w = window, webkit = (!(window.webkitConvertPointFromNodeToPage == null));
+    // Webkit and IE 6-8
+    if ( webkit || w.innerWidth === undefined) {
+      vpw = document.documentElement.clientWidth;
+      vph = document.documentElement.clientHeight;
+    }
+    // Everything else
+    else{
+      vpw =  w.innerWidth;
+      vph =  w.innerHeight;
+    }
+
+    var dims = { width: vpw, height: vph };
+    return dims;
+};
