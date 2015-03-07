faaef856e345119438e48e1086603529ef23522d
Brett Jankord <Brett.Jankord@Cerner.com>
2015-03-07 07:36:03 -0600

    Added doc site

--- viewportwidth.js
+++ viewportwidth.js
@@ -1,9 +1,8 @@
-module.exports = function() {
+var viewportwidth = function() {
   "use strict";
-  var isWebKit = 'WebkitAppearance' in document.documentElement.style;
   var isOldIE = !(typeof (window.innerWidth) == 'number'); // IE 6-8
 
-  if (isWebKit || isOldIE) {
+  if (isOldIE) {
     return document.documentElement.clientWidth;
   }
 
